ab55afdd0f243f00f0c39962f7d576a02b7d8ae7
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-03-18 17:51:54 -0300

    :iphone: feat: Iniciando implementação de responsividade

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+// nav-menu responsivo
+
+$('.nav-menu').origamidMenu({
+	breakpoint: 768,
+	top: 52,
+	background: 'var(--color-secondary)',
+	color: "white"
+});
+
+// $('.internal-nav').origamidMenu({
+// 	breakpoint: 768,
+// 	top: 52,
+// 	background: 'var(--color-secondary)',
+// 	color: "white"
+// });
