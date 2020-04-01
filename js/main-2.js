ce62302f67bd330a7539475f1e76d689ed0e8332
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-04-01 14:18:53 -0300

    Updates

---
+++ js/main-2.js
@@ -0,0 +1,12 @@
+// nav-menu responsivo
+const index = '../index.html';
+
+$('.nav-menu').origamidMenu({
+	breakpoint: 768,
+	top: 52,
+	background: 'var(--color-support-3)',
+	color: "var(--color-primary)"
+});
+
+// simple-anime
+new SimpleAnime();
---
+++ js/main.js
@@ -0,0 +1,12 @@
+// nav-menu responsivo
+const index = '../index.html';
+
+$('.nav-menu').origamidMenu({
+	breakpoint: 768,
+	top: 52,
+	background: 'var(--color-secondary)',
+	color: "white"
+});
+
+// simple-anime
+new SimpleAnime();
