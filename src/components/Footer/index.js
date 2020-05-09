a8ef5db2f0988c5077b23e9644b767981c7c8668
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-05-09 11:03:22 -0300

    :lipstick: feat: Styling Footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,46 +1,70 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import {  } from 'react-router-dom';
-
-import './styles';
+import { Container, Col, Row } from 'react-bootstrap';
+import { FooterWrapper, SocialMedias, SocialMediaItem } from './styles';
+import { SubTitle, Paragraph } from '../../styles/objects/texts';
+import { NavItem, NavLink, NavMenu } from '../../styles/objects/nav';
+// imgs
+import facebookImg from '../../assets/img/icons/001-facebook.svg';
+import instagramImg from '../../assets/img/icons/002-instagram.svg';
+import twitterImg from '../../assets/img/icons/003-twitter.svg';
 
 export default function Footer() {
-    return(
-        <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-sm-4">
-                        <h3 class="footer-titles">Mapa do Site</h3>
-                        <ul class="footer-nav">
-                            <li class="footer-nav-item"><a class="footer-nav-link active" href="index.html">Inicio</a></li>
-                            <li class="footer-nav-item"><a class="footer-nav-link" href="churrascaria.html">Churrascaria</a></li>
-                            <li class="footer-nav-item"><a class="footer-nav-link" href="menu.html">Menu</a></li>
-                            <li class="footer-nav-item"><a class="footer-nav-link" href="sobre.html">Sobre</a></li>
-                            <li class="footer-nav-item"><a class="footer-nav-link" href="contato.html">Contato</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-12 col-sm-4">
-                        <h3 class="footer-titles">Redes Sociais</h3>
-                        <div class="social-medias">
-                            <a class="social-medias-link" href="https://facebook.com" target="blank"><img src="img/icons/001-facebook.svg" alt="Icone Facebook"></a>
-                            <a class="social-medias-link" href="https://instagram.com" target="blank"><img src="img/icons/002-instagram.svg" alt="Icone Instagram"></a>
-                            <a class="social-medias-link" href="https://twitter.com" target="blank"><img src="img/icons/003-twitter.svg" alt="Icone Twitter"></a>
-                        </div>
-                    </div>
-                    <div class="col-12 col-sm-4">
-                        <h3 class="footer-titles">Contatos</h3>
-                        <p class="footer-contatos">
-                            - Telefone: (71) 99999 - 9999<br>
-                            - E-mail: contato@sabordecarne.com.br
-                        </p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <p class="footer-copy">Sabor de Carne 2020 @ Todos os direitos reservados.</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <FooterWrapper>
+      <Container>
+        <Row>
+          <Col xs="12" sm="4">
+            <SubTitle>Mapa do Site</SubTitle>
+            <NavMenu>
+              <NavItem verticalNav>
+                <NavLink to="/">Inicio</NavLink>
+              </NavItem>
+              <NavItem verticalNav>
+                <NavLink to="/">Churrascaria</NavLink>
+              </NavItem>
+              <NavItem verticalNav>
+                <NavLink to="/">Menu</NavLink>
+              </NavItem>
+              <NavItem verticalNav>
+                <NavLink to="/">Sobre</NavLink>
+              </NavItem>
+              <NavItem verticalNav>
+                <NavLink to="/">Contato</NavLink>
+              </NavItem>
+            </NavMenu>
+          </Col>
+          <Col xs="12" sm="4">
+            <SubTitle>Redes Sociais</SubTitle>
+            <SocialMedias>
+              <SocialMediaItem href="https://facebook.com" target="blank">
+                <img src={facebookImg} alt="Icone Facebook" />
+              </SocialMediaItem>
+              <SocialMediaItem href="https://instagram.com" target="blank">
+                <img src={instagramImg} alt="Icone Instagram" />
+              </SocialMediaItem>
+              <SocialMediaItem href="https://twitter.com" target="blank">
+                <img src={twitterImg} alt="Icone Twitter" />
+              </SocialMediaItem>
+            </SocialMedias>
+          </Col>
+          <Col xs="12" sm="4">
+            <SubTitle>Contatos</SubTitle>
+            <Paragraph>
+              - Telefone: (71) 99999 - 9999
+              <br /> - E-mail: contato@sabordecarne.com.br
+            </Paragraph>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="12">
+            <Paragraph style="margin-top: var(--gap-sm);">
+              Sabor de Carne 2020 @ Todos os direitos reservados.
+            </Paragraph>
+          </Col>
+        </Row>
+      </Container>
+    </FooterWrapper>
+  );
 }
-
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,10 +1,27 @@
+/* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 
-export const Footer = styled.footer`
+export const FooterWrapper = styled.footer`
+  & {
+    padding: var(--gap-lg);
+    background: ${(props) => props.background};
+  }
+`;
 
-    & {
-        padding: var(--gap-lg);
-        background: {(props) => props.background};
-    }
+export const SocialMedias = styled.div`
+  & {
+    display: flex;
+    flex-direction: row;
+  }
+`;
 
+export const SocialMediaItem = styled.a`
+  & {
+    padding: 0;
+    margin-right: var(--gap);
+  }
+
+  &:last-child {
+    margin-right: 0;
+  }
 `;
