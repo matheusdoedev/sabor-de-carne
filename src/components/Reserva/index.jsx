460d9d9a862e9b7c8235dd4f5c86122a5f37455b
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-06-18 17:26:05 -0300

    :bug: fix: Fixing for mistake

--- src/components/Reserva/index.jsx
+++ src/components/Reserva/index.jsx
@@ -11,27 +11,27 @@ const FormReserva = () => {
           <Col xs="12" sm={{ span: 6, offset: 3 }}>
             <h2>Faça sua reserva!</h2>
 
-            <label for="nome">Nome</label>
+            <label htmlFor="nome">Nome</label>
             <input type="text" name="nome" id="nome" required />
 
-            <label for="email">E-mail</label>
+            <label htmlFor="email">E-mail</label>
             <input type="email" name="email" id="email" required />
 
-            <label for="telefone">Telefone</label>
+            <label htmlFor="telefone">Telefone</label>
             <input type="tel" name="telefone" id="telefone" required />
 
             <label
-              for="pessoas"
+              htmlFor="pessoas"
               style={{ display: "flex", justifyContent: "space-between" }}
             >
               Quantidade de pessoas*<span>Até no maximo 10 pessoas.</span>
             </label>
             <input type="number" name="pessoas" id="pessoas" required />
 
-            <label for="data">Data</label>
+            <label htmlFor="data">Data</label>
             <input type="datetime-local" name="data" id="data" required />
 
-            <label for="mensagem">Mensagem</label>
+            <label htmlFor="mensagem">Mensagem</label>
             <textarea name="mensagem" id="mensagem" required></textarea>
 
             <ReservarBtn type="submit">Reservar</ReservarBtn>
