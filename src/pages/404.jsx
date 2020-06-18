063c02673797150a1092ffec04f22d8da9d265f0
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-06-18 17:25:47 -0300

    :lipstick: chore: Added 404 page component

---
+++ src/pages/404.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+// components
+import Layout from "../layouts/page";
+import { Container, Row, Col } from "react-bootstrap";
+import SEO from "../components/Seo/index";
+// styles
+import { Page404wrapper } from "../styles/pages/404";
+
+const page404 = () => (
+  <Layout>
+    <SEO title="404" description="Pagina não encontrada" />
+
+    <Page404wrapper>
+      <Container>
+        <Row>
+          <Col xs="12">
+            <h3>Página não encontrada!</h3>
+          </Col>
+        </Row>
+      </Container>
+    </Page404wrapper>
+  </Layout>
+);
+
+export default page404;
---
+++ src/styles/pages/404.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Page404wrapper = styled.section`
+  & {
+    padding: var(--gap-xl) 0;
+
+    h3 {
+      text-align: center;
+    }
+  }
+`;
